2efd7b8378af0bb622929c95ba2fc953ad6667be
Mauli Saxena <maulisaxena.16@gmail.com>
2025-03-11 15:44:05 +0530

    added routes-login,chat,profile

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import Login from './pages/Login/Login'
 import Signup from './Signup'
 import {Route, Routes} from 'react-router-dom'
+import Chat from './pages/Chats/Chat'
+import Profile from './pages/Profile/Profile'
 
 function App() {
  
@@ -14,6 +16,9 @@ function App() {
   
       <Routes>
         <Route path='/' element={<Login/>}/>
+        <Route path='/chat' element={<Chat/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+
 
       </Routes>
     </>
