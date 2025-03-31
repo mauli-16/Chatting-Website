98e16d6826d972d8e3e6e832f2f094fbd6daf676
Mauli Saxena <maulisaxena.16@gmail.com>
2025-04-01 00:02:55 +0530

    right part ui

--- vite-project/src/pages/Chats/Rightpart.jsx
+++ vite-project/src/pages/Chats/Rightpart.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import './Rightpart.css'
 const Rightpart = () => {
   return (
-    <div>Rightpart</div>
+    <div >
+      <div className="circle"></div>
+      <div className="profile">
+      <div className="name">Mauli Saxena</div>
+      <div className="bio">sweet-toothed</div>
+      
+      </div>
+      <div className="media">Media</div>
+      <button className='logout'>Logout</button>
+
+    </div>
   )
 }
 
