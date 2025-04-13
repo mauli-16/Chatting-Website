5a58a369018d63586edc1d89ff1fa76c28048372
Mauli Saxena <maulisaxena.16@gmail.com>
2025-04-13 13:37:26 +0530

    profile ui made

--- vite-project/src/pages/Profile/Profile.jsx
+++ vite-project/src/pages/Profile/Profile.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import './Profile.css'
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { updateProfile } from 'firebase/auth';
+import { auth } from '../../firebase/firebase'
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='profile-box'>
+      <div className="photo-box">
+        <label htmlFor="" className='pp'>Enter your profile photo</label>
+        <input type="file" className='photo'/>
+      </div>
+      <div className="bio">
+      <label htmlFor="" className='pt'>Enter your bio</label>
+      <input type="text" />
+      </div>
+     
+    </div>
   )
 }
 
